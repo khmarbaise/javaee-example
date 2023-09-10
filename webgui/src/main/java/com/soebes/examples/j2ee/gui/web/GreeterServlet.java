5b2b4d871a200870e618eb59c697492108beb4a8
Karl Heinz Marbaise <kama@soebes.de>
2023-09-10 16:42:03 +0200

    Improved.

--- webgui/src/main/java/com/soebes/examples/j2ee/gui/web/GreeterServlet.java
+++ webgui/src/main/java/com/soebes/examples/j2ee/gui/web/GreeterServlet.java
@@ -1,6 +1,7 @@
 package com.soebes.examples.j2ee.gui.web;
 
-import java.io.IOException;
+import com.soebes.examples.j2ee.domain.Name;
+import com.soebes.examples.j2ee.sevice.api.Greeter;
 
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
@@ -8,13 +9,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.soebes.examples.j2ee.domain.Name;
-import com.soebes.examples.j2ee.sevice.api.Greeter;
+import java.io.IOException;
 
 
 @WebServlet(name = "GreeterController", urlPatterns = {"/greet"})
 public class GreeterServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 42L;
 
     @EJB
     private Greeter greeter;
